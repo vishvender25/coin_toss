a24640a212f21770947b70e04bf78dbde23cd061
vishvender25 <102175447+vishvender25@users.noreply.github.com>
2023-10-13 16:48:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
-var user_choice = prompt("Enter your choice heads/tails");
-var computer_choice = Math.random();
-computer_choice *= 2;
-computer_choice = Math.ceil(computer_choice);
-// computer_choice 1 -> heads and computer_choice 2 -> tails
-if(computer_choice == 1){
-    document.getElementById("coin").setAttribute("src", "./images/head.png");
-}
-
-else{
-    document.getElementById("coin").setAttribute("src", "./images/tail.png");
-}
-document.getElementById("user-choice").innerText = "Your choice " + user_choice;
-
-if(computer_choice == 1 && user_choice == 'heads' ){
-
-    document.getElementById("result").innerText = "You won!!";
-}
-else if(computer_choice == 2 && user_choice == 'tails'){
-    document.getElementById("result").innerText = "You won!!";
-}
-else{
-    document.getElementById("result").innerText = "You Loose!!";
-}
+var computer_choice = "";
+var user_choice = "";
+document.getElementById("submit-btn").addEventListener("click", function(event){
+    user_choice = document.getElementById("user-input").value;
+    if(user_choice == "heads"){
+        document.querySelectorAll(".coin")[0].setAttribute("src", "./images/head.png");
+    
+    }
+    else{
+        document.querySelectorAll(".coin")[0].setAttribute("src", "./images/tail.png");
+    }
+    
+    var computer_choice = Math.random();
+    computer_choice *=2 ;
+    computer_choice = Math.ceil(computer_choice);
+    
+    var coin_choice = "";
+    
+    if(computer_choice == 1){
+        coin_choice = "heads";
+        document.querySelectorAll(".coin")[1].setAttribute("src", "./images/head.png");
+    }
+    
+    else{
+        coin_choice = "tails";
+        document.querySelectorAll(".coin")[1].setAttribute("src", "./images/tail.png");
+    }
+    if(user_choice == coin_choice){
+        document.getElementById("result").innerText = "You Win!!";
+    }
+    else{
+        document.getElementById("result").innerText = "You Loose!!";
+    }
+});
